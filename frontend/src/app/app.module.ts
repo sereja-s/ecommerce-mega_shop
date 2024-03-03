9878c50fa04ccd7c895cdbcf057d5c8e6b483b22
sereja-s <serejasuvorov1979@gmail.com>
2024-03-03 20:48:26 +0300

    выпадающий список корзины в header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
 	  BrowserModule,
 	  BrowserAnimationsModule,
-	 AppRoutingModule,
+	  AppRoutingModule,
+	  NgbModule,
 	  HttpClientModule,
 	  NgxSpinnerModule,	  
 	  ToastrModule.forRoot(),
